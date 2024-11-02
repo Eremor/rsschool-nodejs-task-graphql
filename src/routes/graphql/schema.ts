987280e73ca0ba6385f8eddbbad9a4f29a7eefb6
Artem Smirnov <artem.s13@gmail.com>
2024-11-02 18:39:26 +0300

    feat: implement get all resources

--- src/routes/graphql/schema.ts
+++ src/routes/graphql/schema.ts
@@ -1,6 +1,93 @@
-import { GraphQLSchema } from 'graphql';
+import {
+  GraphQLBoolean,
+  GraphQLEnumType,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString
+} from 'graphql';
+import { MemberTypeId } from '../member-types/schemas.js';
+import { UUIDType } from './types/uuid.js';
+
+const MemberTypeIdEnum = new GraphQLEnumType({
+  name: 'MemberTypeId',
+  values: {
+    BASIC: {
+      value: MemberTypeId.BASIC
+    },
+    BUSINESS: {
+      value: MemberTypeId.BUSINESS
+    }
+  }
+})
+
+const MemberType = new GraphQLObjectType({
+  name: 'MemberType',
+  fields: {
+    id: { type: new GraphQLNonNull(MemberTypeIdEnum) },
+    discount: { type: new GraphQLNonNull(GraphQLFloat) },
+    postsLimitPerMonth: { type: new GraphQLNonNull(GraphQLInt) }
+  }
+})
+
+const PostType = new GraphQLObjectType({
+  name: 'Post',
+  fields: {
+    id: { type: new GraphQLNonNull(UUIDType) },
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: new GraphQLNonNull(GraphQLString) }
+  }
+})
+
+const ProfileType = new GraphQLObjectType({
+  name: 'Profile',
+  fields: {
+    id: { type: new GraphQLNonNull(UUIDType) },
+    isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
+    yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
+    memberType: { type: new GraphQLNonNull(MemberType) }
+  }
+})
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    balance: { type: new GraphQLNonNull(GraphQLFloat) },
+    profile: { type: ProfileType },
+    posts: { type: new GraphQLList(new GraphQLNonNull(PostType)) },
+    userSubscribedTo: { type: new GraphQLList(new GraphQLNonNull(UserType)) },
+    subscribedToUser: { type: new GraphQLList(new GraphQLNonNull(UserType)) }
+  })
+})
+
+const RootQueryType = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    memberTypes: {
+      type: new GraphQLList(new GraphQLNonNull(MemberType)),
+      resolve: async (_source, _args, { prisma }) => prisma.memberType.findMany()
+    },
+    posts: {
+      type: new GraphQLList(new GraphQLNonNull(PostType)),
+      resolve: async (_source, _args, { prisma }) => prisma.post.findMany()
+    },
+    users: {
+      type: new GraphQLList(new GraphQLNonNull(UserType)),
+      resolve: async (_source, _args, { prisma }) => prisma.user.findMany()
+    },
+    profiles: {
+      type: new GraphQLList(new GraphQLNonNull(ProfileType)),
+      resolve: async (_source, _args, { prisma }) => prisma.profile.findMany()
+    }
+  }
+})
 
 export const schema = new GraphQLSchema({
-  query: null,
+  query: RootQueryType,
   mutation: null
 })
