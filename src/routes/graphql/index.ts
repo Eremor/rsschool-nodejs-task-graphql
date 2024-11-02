d58ed96e3142851a724a220b6b72e5109b96b917
Artem Smirnov <artem.s13@gmail.com>
2024-11-02 23:30:13 +0300

    feat: implement query depth limit

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,10 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
-import { schema } from './schema.js'
+import { graphql, parse, validate } from 'graphql';
+import { schema } from './schema.js';
+import depthLimit from 'graphql-depth-limit';
+
+const DEPTH_LIMIT_VALUE = 5;
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -18,6 +21,18 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     async handler(req) {
       try {
         const { query, variables } = req.body;
+
+        const parsedQuery = parse(query)
+
+        const validationErrors = validate(schema, parsedQuery, [depthLimit(DEPTH_LIMIT_VALUE)])
+
+        if (!!validationErrors) {
+          return {
+            errors: validationErrors.map((err) => ({
+              message: err.message
+            }))
+          }
+        }
         
         const result = await graphql({
           schema,
@@ -29,12 +44,14 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         })
 
         if (result.errors) {
-          return { errors: result.errors, prisma }
+          return { errors: result.errors }
         }
   
         return result;
-      } catch (error) {
-        return { errors: [{ message: 'Internal server error' }], prisma }
+      } catch {
+        return {
+          errors: [{ message: 'Internal server error' }]
+        }
       }
     },
   });
